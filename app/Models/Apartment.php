8eac047da5bbf3548234e98886e74c568e66fdc6
nekrosb <makarov.nikita.mn@gmail.com>
2026-06-17 12:06:13 +0200

    feat: define table relationships in corresponding models

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -4,10 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\City;
 
 class Apartment extends Model
 {
     /** @use HasFactory<\Database\Factories\ApartmentFactory> */
     use HasFactory;
-    protected $fillable = ['title', 'description', 'address', 'price', 'city_id'];
+    protected $fillable = ['title', 'description', 'address', 'price', 'bedrooms', 'bathrooms', 'user_id', 'city_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
 }
--- app/Models/Image.php
+++ app/Models/Image.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+    protected $fillable = ['image_path', 'apartment_id'];
+
+    public function apartment()
+    {
+        return $this->belongsTo(Apartment::class);
+    }
 }
